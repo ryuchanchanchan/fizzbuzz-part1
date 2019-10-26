256a098c8255b6f9f2a3f562d2395ea1dffc7a44
Ryu <kuromamekikaku@gmail.com>
2019-10-26 11:51:17 +0900

    FizzBuzz変換を関数化

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,12 +1,16 @@
-number = int(input("1つの自然数を入れてね: "))
+def fizzbuzz_convert(number):
+    if number % 15 == 0:
+        print("FizzBuzz")
+    elif number % 5 == 0:
+        print("Buzz")
+    elif number % 3 == 0:
+        print("Fizz")
+    else:
+        print(str(number))
 
-if number % 15 == 0:
-    print("FizzBuzz")
-elif number % 5 == 0:
-    print("Buzz")
-elif number % 3 == 0:
-    print("Fizz")
-else:
-    print(str(number))
 
-print(number)
+fizzbuzz_convert(1)
+fizzbuzz_convert(2)
+fizzbuzz_convert(5)
+fizzbuzz_convert(15)
+fizzbuzz_convert(100)
